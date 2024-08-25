e9e7be2631e88a1405891244ddf40832fe33e494
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-25 19:20:10 -0300

    caminho correto

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from projeto.views import tabela
+from projeto.views.endpoint.lista_Json import lista
 
 urlpatterns = [
     path('', tabela, name='tabela'),
+    path('lista/produtos/',lista, name="lista_produto"),
 ]
