a06fe14e294c7c7e95dfc0fc204145cab0babbff
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-25 17:30:05 -0300

    models criado

--- projeto/admin.py
+++ projeto/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from projeto.models import *
+
+admin.site.register(Categoria)
+admin.site.register(Produto)
--- projeto/models.py
+++ projeto/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    index = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=45)
+
+    def __str__(self):
+        return self.nome
+class Produto(models.Model):
+    index = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=60)
+    descricao = models.CharField(max_length=200)
+    preco = models.IntegerField()
+    ativo = models.BooleanField(default=False)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    usuario =  models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
